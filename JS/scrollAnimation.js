7988b87a0cc8f6654fc913c7b6783643f8ebe3af
Sandeepa D.S <supundunusingha2@gmail.com>
2023-08-11 20:09:30 +0530

    scroll animations added

---
+++ JS/scrollAnimation.js
@@ -0,0 +1,35 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+function revealLoad() {
+  var reveals = document.querySelectorAll(".reveal-load");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active-load");
+    } else {
+      reveals[i].classList.remove("active-load");
+    }
+  }
+}
+
+window.addEventListener("load", revealLoad);
